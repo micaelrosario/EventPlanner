39f61eadfdbc1e48ce3ca926279f126a6331c792
Micael Rosário <micaeldevrosario@gmail.com>
2024-05-07 20:34:50 -0300

    Form and structure of the project created

--- src/main/java/com/example/eventoapp/controllers/indexController.java
+++ src/main/java/com/example/eventoapp/controllers/indexController.java
@@ -1,4 +1,4 @@
-package com.example.eventoapp;
+package com.example.eventoapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
